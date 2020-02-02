980bcb30f561c8af658092df16a27721adf161ce
Nathen Bowes <nathen.bowes@shopify.com>
2020-02-02 10:28:31 -0800

    add more content

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+export default () => (
+  <Fragment>
+    Words by <Styled.a href="http://github.com/NBowes">Nathen Bowes</Styled.a>.
+    <br />
+    aka nape, aka n8, aka napen
+  </Fragment>
+)
